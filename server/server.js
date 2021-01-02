40047652243683f642fd02631e788e3dc3cf6c97
ditheca <75103017+ditheca@users.noreply.github.com>
2021-01-02 02:54:32 -0700

    Prepare for initial deployment

--- server/server.js
+++ server/server.js
@@ -3,17 +3,26 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import lyspRoutes from './routes/lyspRoutes.js'
 import { notFound, asyncErrorHandler } from './middleware/errorMiddleware.js'
+import path from 'path'
 
 dotenv.config()
 connectDB()
 
 const app = express()
 
-app.get('/', (req, res) => res.send('API Running'))
-
 app.get('/api', (req, res) => res.send([{ name: 'Api running' }]))
 
 app.use('/api/lysp', lyspRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve()
+  app.use(express.static(path.join(__dirname, '/client/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => res.send('API Running'))
+}
 
 app.use(notFound)
 app.use(asyncErrorHandler)
